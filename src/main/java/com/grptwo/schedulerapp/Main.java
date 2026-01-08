528f434381533cf11bb21630d2fb2db1e9454862
YUDONG CHEN <24074901@siswa.um.edu.my>
2026-01-08 13:07:51 +0800

    Added window drag functionality

--- src/main/java/com/grptwo/schedulerapp/Main.java
+++ src/main/java/com/grptwo/schedulerapp/Main.java
@@ -6,12 +6,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+import javafx.scene.Parent;
+import javafx.stage.StageStyle;
 public class Main extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/onboarding.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 902, 1255);
+        Parent root = fxmlLoader.load();
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+        Scene scene = new Scene(root, 902, 1255);
         scene.getStylesheets().add(Main.class.getResource("css/style.css").toExternalForm());
         stage.setScene(scene);
         stage.setTitle("Welcome!");
