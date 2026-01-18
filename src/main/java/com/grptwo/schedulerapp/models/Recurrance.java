982ede295565879c9db022c344eecab87eef0534
rach <bbazzuraa@gmail.com>
2026-01-18 13:41:05 +0800

    removed unused codes

--- src/main/java/com/grptwo/schedulerapp/models/Recurrance.java
+++ src/main/java/com/grptwo/schedulerapp/models/Recurrance.java
@@ -3,48 +3,26 @@ package com.grptwo.schedulerapp.models;
 import java.time.LocalDateTime;
 
 public class Recurrance {
-    private Integer recId;
     private Integer interval;
     private Integer times;
     private LocalDateTime endDateTime;
 
     public Recurrance(Integer recId, Integer interval, Integer times, LocalDateTime endDateTime) {
-        this.recId = recId;
         this.interval = interval;
         this.times = times;
         this.endDateTime = endDateTime;
-    }
-
-    public Integer getRecId() {
-        return recId;
-    }
-
-    public void setRecId(Integer recId) {
-        this.recId = recId;
     }
 
     public Integer getInterval() {
         return interval;
     }
 
-    public void setInterval(Integer interval) {
-        this.interval = interval;
-    }
-
     public Integer getTimes() {
         return times;
     }
 
-    public void setTimes(Integer times) {
-        this.times = times;
-    }
-
     public LocalDateTime getEndDateTime() {
         return endDateTime;
-    }
-
-    public void setEndDateTime(LocalDateTime endDateTime) {
-        this.endDateTime = endDateTime;
     }
 }
 
